e1f9c69cd4a25f7fb832a8deb907f4e6ce749240
mina-alabd <49130999+mina-alabd@users.noreply.github.com>
2023-06-26 09:38:28 +0300

    Update key.php

--- key.php
+++ key.php
@@ -160,7 +160,7 @@
         "isGif": true,
         "name": "ayman1",
         "previewUrl": "https://github.com/mina-alabd/monn/raw/master/ayman1.gif",
-        "title":"اميراتي",
+        "title":"amira12",
         "vercode": 1
       }
 
